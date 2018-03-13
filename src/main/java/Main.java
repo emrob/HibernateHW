11cec809a85e0dd82052e699f240b9c004727cb1
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-13 09:26:56 +0000

    more orders added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,14 @@ public class Main {
     public static void main(String[] args) {
         CoffeeOrder coffeeOrder1 = new CoffeeOrder("Soy Latte");
         DBHelper.save(coffeeOrder1);
-
+        CoffeeOrder coffeeOrder2 = new CoffeeOrder("Americano");
+        DBHelper.save(coffeeOrder2);
         Customer customer1 = new Customer("Mark",coffeeOrder1);
         DBHelper.save(customer1);
+        Customer customer2 = new Customer("Emma",coffeeOrder2);
+        DBHelper.save(customer2);
 
         List<Customer> customers = DBHelper.getAll("Customer");
-
+        List<CoffeeOrder> coffeeOrders = DBHelper.getAll("CoffeeOrder");
     }
 }
